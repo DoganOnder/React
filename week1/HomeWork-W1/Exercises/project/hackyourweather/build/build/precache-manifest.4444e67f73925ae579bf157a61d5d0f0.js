795eba37c1ac25eb301f6733f1115ac623d65efa
DoganOnder <61209132+DoganOnder@users.noreply.github.com>
2020-07-11 18:33:35 +0200

    Add files via upload

---
+++ week1/HomeWork-W1/Exercises/project/hackyourweather/build/build/precache-manifest.4444e67f73925ae579bf157a61d5d0f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4c53be4054ca034a973ce3532ae78fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef33163342a70d29e4bf",
+    "url": "/static/css/main.765164e3.chunk.css"
+  },
+  {
+    "revision": "715b7071d867d31b4320",
+    "url": "/static/js/2.19a2c95f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.19a2c95f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef33163342a70d29e4bf",
+    "url": "/static/js/main.af62db1f.chunk.js"
+  },
+  {
+    "revision": "a3d1c8d08193b361a9d4",
+    "url": "/static/js/runtime-main.e0ee316b.js"
+  }
+]);
